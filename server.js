78ee116c122ce93a0d11091dfa8887fec47820c4
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-06-16 19:07:24 -0400

    Updated userRoute.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
+// app.use(routes);
 
 //SEQUELIZE
 
