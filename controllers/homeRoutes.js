1e6b6779aafe21052750bc619ca4b544802d1e50
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-06-16 20:41:23 -0400

    Updated

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,7 @@ const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   try {
-    // Get all projects and JOIN with user data
+    // Get all recipe and JOIN with user data
     const recipeData = await Recipe.findAll({
       include: [
         {
