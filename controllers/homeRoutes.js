ee99b3a4cd68d3f8fa33268c03fc1ed2dd941815
JuneXll <71652307+JuneXll@users.noreply.github.com>
2021-06-22 17:14:25 -0400

    Deleted unneeded code

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -81,12 +81,4 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-// router.get('/homepage', async (req, res) => {
-//   try {
-//    res.render('homepage');
-//  } catch (err) {
-//    res.status(500).json(err);
-//  }
-// });
-
 module.exports = router;
