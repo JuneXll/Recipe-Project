91a7c339f654758480eb77031dcc14d72b2c1aee
Fernando Zaldivar <76067527+Alkathos@users.noreply.github.com>
2021-06-21 20:25:47 -0400

    Added code for recipe retrieval

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -33,24 +33,24 @@ router.get('/recipe/:id', async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
-  try {
-    // Find the logged in user based on the session ID
-    const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Recipe }],
-    });
+// router.get('/profile', withAuth, async (req, res) => {
+//   try {
+//     // Find the logged in user based on the session ID
+//     const userData = await User.findByPk(req.session.user_id, {
+//       attributes: { exclude: ['password'] },
+//       include: [{ model: Recipe }],
+//     });
 
-    const user = userData.get({ plain: true });
+//     const user = userData.get({ plain: true });
 
-    res.render('profile', {
-      ...user,
-      logged_in: true
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+//     res.render('profile', {
+//       ...user,
+//       logged_in: true
+//     });
+//   } catch (err) {
+//     res.status(500).json(err);
+//   }
+// });
 
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
@@ -62,12 +62,12 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-router.get('/homepage', async (req, res) => {
-  try {
-   res.render('homepage');
- } catch (err) {
-   res.status(500).json(err);
- }
-});
+// router.get('/homepage', async (req, res) => {
+//   try {
+//    res.render('homepage');
+//  } catch (err) {
+//    res.status(500).json(err);
+//  }
+// });
 
 module.exports = router;
--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,14 +1,25 @@
 const appId = "5fd9425f";
 const appKey = "51f65fc67c6c78bf473190a7e3c1435b";
 let searchSubmitButton = document.querySelector('#search-submit');
+const url = "https://api.edamam.com/api/recipes/v2?type=public&q=";
 
 let getRequest = function() {
     let userReq = document.querySelector('#search-input').value;
     console.log(userReq);
-    fetch(`https://api.edamam.com/api/recipes/v2?type=public&q=${userReq}&app_id=${appId}&app_key=${appKey}`)
+    fetch(`${url}${userReq}&app_id=${appId}&app_key=${appKey}`)
 .then(res => res.json())
-.then(data => console.log(data));
+.then(data => console.log(data))
+.then((data) => {
+    appendRecipeUrl(data);
+})
+.catch((err) => {
+    console.log('error: ' + err);
+})
 };
+
+const appendRecipeUrl = (data) => {
+    
+}
 
 searchSubmitButton.addEventListener("click", () => {
     getRequest();
